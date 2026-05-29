7395f9098a195d8bb0e7cd66afe9d7d9267f3300
numshv <noumisyifa@gmail.com>
2026-05-29 15:55:46 +0700

    fix: updat footer info

--- src/components/footer/FooterItem.vue
+++ src/components/footer/FooterItem.vue
@@ -11,7 +11,7 @@
             </div>
           </div>
           <p class="mb-4">
-            Kota Bandung, Coblong Sekretariat IOM-ITB Gedung Kampus Center Timur ITB Lantai 2 Jl. Ganesha No. 10 Kec. Coblong 40132.
+            Sekretariat IOM-ITB Gedung Kampus Center Timur ITB Lantai 2 Jl. Ganesha No. 10 Kec. Coblong, Bandung 40132.
           </p>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4">
@@ -24,15 +24,26 @@
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4">
           <h5 class="font-semibold mb-2">KONTAK</h5>
-          <p>sekretariat.iom.itb@gmail.com</p>
-          <p>+62 856-2465-4990</p>
-          <div class="flex space-x-4 mt-2">
-            <a target="_blank" href="https://www.instagram.com/iom_itb" class="w-[18px] h-[18px]"><img :src="require('@/assets/icon/instagram.svg')"/></a>
-            <a href="#" class="w-[18px] h-[18px]"><img :src="require('@/assets/icon/facebook.svg')"/></a>
-            <a href="#" class="w-[18px] h-[18px]"><img :src="require('@/assets/icon/line.svg')"/></a>
-            <a href="#" class="w-[18px] h-[18px]"><img :src="require('@/assets/icon/twitter.svg')"/></a>
-            <a target="_blank" href="https://youtube.com/@iom-itb" class="w-[18px] h-[18px]"><img :src="require('@/assets/icon/youtube.svg')"/></a>
-          </div>
+          <a target="_blank" href="https://mail.google.com/mail/?view=cm&to=info@iom-itb.id" class="flex items-center gap-2 mb-1 hover:underline">
+            <img :src="require('@/assets/icon/email.svg')" class="w-[18px] h-[18px] icon-blue"/>
+            <span>info@iom-itb.id</span>
+          </a>
+          <a target="_blank" href="https://wa.me/6285624654990" class="flex items-center gap-2 mb-1 hover:underline">
+            <img :src="require('@/assets/icon/whatsapp.svg')" class="w-[18px] h-[18px] icon-blue"/>
+            <span>+62 856-2465-4990</span>
+          </a>
+          <a target="_blank" href="https://wa.me/6287854019415" class="flex items-center gap-2 mb-1 hover:underline">
+            <img :src="require('@/assets/icon/whatsapp.svg')" class="w-[18px] h-[18px] icon-blue"/>
+            <span>+62 878-5401-9415</span>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/iom_itb" class="flex items-center gap-2 mb-1 hover:underline">
+            <img :src="require('@/assets/icon/instagram.svg')" class="w-[18px] h-[18px] icon-blue"/>
+            <span>iom_itb</span>
+          </a>
+          <a target="_blank" href="https://youtube.com/@iom-itb" class="flex items-center gap-2 mb-1 hover:underline">
+            <img :src="require('@/assets/icon/youtube.svg')" class="w-[18px] h-[18px] icon-blue"/>
+            <span>@iom-itb</span>
+          </a>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4">
           <h5 class="font-semibold mb-2">GABUNG BERSAMA KAMI</h5>
@@ -55,6 +66,10 @@ export default {
 </script>
 
 <style scoped>
+.icon-blue {
+  filter: brightness(0) saturate(100%) invert(15%) sepia(61%) saturate(1200%) hue-rotate(187deg) brightness(90%) contrast(102%);
+}
+
 footer ul {
   list-style: none;
   padding: 0;
